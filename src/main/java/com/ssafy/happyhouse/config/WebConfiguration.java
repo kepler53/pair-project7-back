6f4d1ffd7af659b9ec437e1405f68f63b3543d92
이종현 <dlwhdgus53@naver.com>
2021-05-21 22:44:08 +0900

    CORS 해결

---
+++ src/main/java/com/ssafy/happyhouse/config/WebConfiguration.java
@@ -0,0 +1,23 @@
+package com.ssafy.happyhouse.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+	
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("*")
+				.allowedHeaders("X-AUTH-TOKEN","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Credentials")
+				.exposedHeaders("Content-Disposition","X-AUTH-TOKEN","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Credentials")
+				.allowCredentials(false).maxAge(3600);
+	}
+	
+
+}
--- src/main/java/com/ssafy/happyhouse/config/WebSecurityConfig.java
+++ src/main/java/com/ssafy/happyhouse/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.ssafy.happyhouse.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -51,15 +52,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
+		
+	
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/jwt/**","/user/enroll").permitAll().
+				.authorizeRequests().antMatchers("/jwt/**","/user/enroll").permitAll()
+				// options method allow
+				.antMatchers(HttpMethod.OPTIONS).permitAll()
 				// all other requests need to be authenticated
-				anyRequest().authenticated().and().
+				.anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
 				// store user's state.
 				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
