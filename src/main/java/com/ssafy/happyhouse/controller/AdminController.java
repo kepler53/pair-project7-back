80f94ecf3f25e952702a18fdc125b6f7c389c226
이종현 <dlwhdgus53@naver.com>
2021-05-27 13:15:35 +0900

    매물추가 관련 컨트롤러

--- src/main/java/com/ssafy/happyhouse/controller/AdminController.java
+++ src/main/java/com/ssafy/happyhouse/controller/AdminController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.happyhouse.dto.HouseAddDto;
 import com.ssafy.happyhouse.dto.HouseDealDto;
+import com.ssafy.happyhouse.dto.HouseRentDto;
 import com.ssafy.happyhouse.service.HouseService;
 
 @RestController
@@ -34,84 +35,92 @@ public class AdminController {
 	@Autowired
 	private HouseService houseService;
 	
+//	@PostMapping
+//	public ResponseEntity<String> addHouseDeal(@RequestBody HouseAddDto houseAddDto, Principal principal){
+//		
+//		String name = principal.getName();
+//		System.out.println(houseAddDto);
+//		
+//		String address = houseAddDto.getCity() + " " + houseAddDto.getGu() + " "+ houseAddDto.getDong();
+////		System.out.println(address);
+//		
+//		
+//		String lat = "";
+//		String lng = "";
+//		
+//        try {
+//            String addr = URLEncoder.encode(address, "UTF-8");  //주소입력
+//            String apiURL = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=" + addr; //json
+//            //String apiURL = "https://openapi.naver.com/v1/map/geocode.xml?query=" + addr; // xml
+//            URL url = new URL(apiURL);
+//            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+//            con.setRequestMethod("GET");
+//            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", "09vzyngctu");
+//            con.setRequestProperty("X-NCP-APIGW-API-KEY", "z1VHkKMWkWpnIhz7alV3oXpysduy3SSUgvkXdIEb");
+//            int responseCode = con.getResponseCode();
+//            BufferedReader br;
+//            if(responseCode==200) { 
+//                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+//            } else {  
+//                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+//            }
+//            String inputLine;
+//            StringBuffer response = new StringBuffer();
+//            while ((inputLine = br.readLine()) != null) {
+//                response.append(inputLine);
+//            }
+//            br.close();
+//
+//            String result = response.toString();
+////          System.out.println(result);
+//            JSONParser jsonParser = new JSONParser();
+//            JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
+//            
+//            JSONArray addresses = (JSONArray) jsonObject.get("addresses");
+////            int size = addresses.size();
+////            System.out.println(addresses);
+//            for (int i = 0; i < addresses.size(); i++) {
+//				JSONObject object = (JSONObject) addresses.get(i);
+//				
+//				System.out.println(object.get("x"));
+//				System.out.println(object.get("y"));
+//				
+//				lat = (String)object.get("y");
+//				lng = (String)object.get("x");
+//				
+////				mapDto.setLatitude((String)object.get("y"));
+////				mapDto.setLongtitude((String)object.get("x"));
+//			}
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
+//		
+//        HouseDealDto houseDealDto = new HouseDealDto();
+//        houseDealDto.setDong(houseAddDto.getDong());
+//		houseDealDto.setAptName(houseAddDto.getAptName());
+//		houseDealDto.setDealAmount(houseAddDto.getDeposit());
+//		houseDealDto.setBuildYear(houseAddDto.getBuildYear());
+//		houseDealDto.setArea(houseAddDto.getArea());
+//		houseDealDto.setFloor(houseAddDto.getFloor());
+//		houseDealDto.setRentMoney(houseAddDto.getFee());
+//		houseDealDto.setLat(lat);
+//		houseDealDto.setLng(lng);
+//		
+//		
+//		String result = houseService.addHouseDeal(houseDealDto);
+//		System.out.println(result);
+//		return new ResponseEntity<String>(result,HttpStatus.OK);
+//	}
+	
+	
 	@PostMapping
-	public ResponseEntity<String> addHouseDeal(@RequestBody HouseAddDto houseAddDto, Principal principal){
-		
-		String name = principal.getName();
-		System.out.println(houseAddDto);
-		
-		String address = houseAddDto.getCity() + " " + houseAddDto.getGu() + " "+ houseAddDto.getDong();
-//		System.out.println(address);
+	public ResponseEntity<String> addHouserRent(@RequestBody HouseRentDto houseRentDto){
 		
 		
-		String lat = "";
-		String lng = "";
-		
-        try {
-            String addr = URLEncoder.encode(address, "UTF-8");  //주소입력
-            String apiURL = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=" + addr; //json
-            //String apiURL = "https://openapi.naver.com/v1/map/geocode.xml?query=" + addr; // xml
-            URL url = new URL(apiURL);
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", "09vzyngctu");
-            con.setRequestProperty("X-NCP-APIGW-API-KEY", "z1VHkKMWkWpnIhz7alV3oXpysduy3SSUgvkXdIEb");
-            int responseCode = con.getResponseCode();
-            BufferedReader br;
-            if(responseCode==200) { 
-                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            } else {  
-                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-            }
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = br.readLine()) != null) {
-                response.append(inputLine);
-            }
-            br.close();
-
-            String result = response.toString();
-//          System.out.println(result);
-            JSONParser jsonParser = new JSONParser();
-            JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
-            
-            JSONArray addresses = (JSONArray) jsonObject.get("addresses");
-//            int size = addresses.size();
-//            System.out.println(addresses);
-            for (int i = 0; i < addresses.size(); i++) {
-				JSONObject object = (JSONObject) addresses.get(i);
-				
-				System.out.println(object.get("x"));
-				System.out.println(object.get("y"));
-				
-				lat = (String)object.get("y");
-				lng = (String)object.get("x");
-				
-//				mapDto.setLatitude((String)object.get("y"));
-//				mapDto.setLongtitude((String)object.get("x"));
-			}
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-		
-        HouseDealDto houseDealDto = new HouseDealDto();
-        houseDealDto.setDong(houseAddDto.getDong());
-		houseDealDto.setAptName(houseAddDto.getAptName());
-		houseDealDto.setDealAmount(houseAddDto.getDeposit());
-		houseDealDto.setBuildYear(houseAddDto.getBuildYear());
-		houseDealDto.setArea(houseAddDto.getArea());
-		houseDealDto.setFloor(houseAddDto.getFloor());
-		houseDealDto.setRentMoney(houseAddDto.getFee());
-		houseDealDto.setLat(lat);
-		houseDealDto.setLng(lng);
-		
-		
-		String result = houseService.addHouseDeal(houseDealDto);
-		System.out.println(result);
-        
-        
+		String result = houseService.addHouseRent(houseRentDto);
 		
 		return new ResponseEntity<String>(result,HttpStatus.OK);
+		
 		
 	}
 
